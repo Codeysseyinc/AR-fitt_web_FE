e37ea94119c7822d16c189494e50a054055d6d02
hurriyanasir <82242392+hurriyanasir@users.noreply.github.com>
2024-04-13 04:08:37 +0500

    Initial commit for signup and landing page

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
+  theme: {
+    extend: {
+      colors: {
+        primarySaturated: "#648789",
+        primaryLight: "#41B4BA",
+        primary: "#408589",
+        primaryDark: "#07545A",
+        contrastText: "#fff",
+        white: "#fff",
+        pink: "#D66F75",
+        purple: "#7F84C2",
+        link: "#503ADA",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
