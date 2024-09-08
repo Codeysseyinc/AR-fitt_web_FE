4ff1f08e4078cd128faea7982b3f3a8d685fd693
hurriyanasir <hurriyanasir@gmail.com>
2024-09-08 05:45:25 +0500

    fix scroll

--- src/components/navBar/navbar.tsx
+++ src/components/navBar/navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
   });
   return (
     <Grid className="flex flex-row absolute w-full bg-[#C8C8C8]  bg-opacity-20 h-[8%] z-40">
-      <div className=" w-[30%] flex">
+      <div className=" w-[30%]">
         <img
           src="/assets/images/logo.png"
           alt="logo"
