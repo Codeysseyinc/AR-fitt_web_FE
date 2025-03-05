5ad439a4e2eea869e62a45e6081b6c082f1fb067
Rafay-dot <rafayrashed130@gmail.com>
2025-03-06 02:56:58 +0500

    rafay: implemented lazy loading on app.tsx to shorten bundle size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, lazy, Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -6,18 +6,22 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { ARfittProvider } from "./context/storeContext";
 import HTTPService from "./services/base.service";
-import SignUp from "./pages/SignUp";
-import LogIn from "./pages/LogIn/logIn";
-import HomePage from "./pages/Home page";
-import HomeLayout from "./layouts/homePageLayout";
-import GetStarted from "./pages/LogIn/getStarted";
-import CategoryPage from "./pages/SignUp/categoryPage";
-import ResetPassword from "./pages/LogIn/resetPassword";
-import ForgotPassword from "./pages/LogIn/forgotPassword";
 import LandingPage from "./pages/Landing page/landingPage";
-import ItemDescription from "./pages/Home page/itemDescription";
-import SuggestedItems from "./pages/Home page/SuggestedItems/suggestedItems";
 import CONSTANTS from "./utils/constants/CONSTANTS";
+
+// Lazy-loaded components
+const SignUp = lazy(() => import("./pages/SignUp"));
+const LogIn = lazy(() => import("./pages/LogIn/logIn"));
+const HomePage = lazy(() => import("./pages/Home page"));
+const HomeLayout = lazy(() => import("./layouts/homePageLayout"));
+const GetStarted = lazy(() => import("./pages/LogIn/getStarted"));
+const CategoryPage = lazy(() => import("./pages/SignUp/categoryPage"));
+const ResetPassword = lazy(() => import("./pages/LogIn/resetPassword"));
+const ForgotPassword = lazy(() => import("./pages/LogIn/forgotPassword"));
+const ItemDescription = lazy(() => import("./pages/Home page/itemDescription"));
+const SuggestedItems = lazy(
+  () => import("./pages/Home page/SuggestedItems/suggestedItems")
+);
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -36,20 +40,22 @@ const App = () => {
         <PersistGate loading={null} persistor={persistor}>
           <ARfittProvider>
             <Router>
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<LogIn />} />
-                <Route path="/getStarted" element={<GetStarted />} />
-                <Route path="/forgotPassword" element={<ForgotPassword />} />
-                <Route path="/resetPassword" element={<ResetPassword />} />
-                <Route path="/categoryPage" element={<CategoryPage />} />
-                <Route path="/home" element={<HomeLayout />}>
-                  <Route index element={<HomePage />} />
-                  <Route path="suggestion" element={<SuggestedItems />} />
-                  <Route path="item" element={<ItemDescription />} />
-                </Route>
-              </Routes>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/login" element={<LogIn />} />
+                  <Route path="/getStarted" element={<GetStarted />} />
+                  <Route path="/forgotPassword" element={<ForgotPassword />} />
+                  <Route path="/resetPassword" element={<ResetPassword />} />
+                  <Route path="/categoryPage" element={<CategoryPage />} />
+                  <Route path="/home" element={<HomeLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="suggestion" element={<SuggestedItems />} />
+                    <Route path="item" element={<ItemDescription />} />
+                  </Route>
+                </Routes>
+              </Suspense>
             </Router>
           </ARfittProvider>
         </PersistGate>
